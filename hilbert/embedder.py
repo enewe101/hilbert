Add Cautious Nesterov Solver Add tests for solvers Correct the ordering of V and W in gradient call signature and return. Add similarity function for two vectors for a quick and dirty check of embeddings.

hilbert/embedder.py
@@ -1,6 +1,15 @@
 import hilbert as h
 import numpy as np
 
+def sim(word, context, embedder, dictionary):
+    word_id = dictionary.get_id(word)
+    context_id = dictionary.get_id(context)
+    word_vec, context_vec = embedder.W[word_id], embedder.W[context_id]
+    product = np.dot(word_vec, context_vec) 
+    word_norm = np.linalg.norm(word_vec)
+    context_norm =  np.linalg.norm(context_vec)
+    return product / (word_norm * context_norm)
+
 
 # TODO: enable sharding
 class HilbertEmbedder(object):
@@ -63,21 +72,14 @@ class HilbertEmbedder(object):
         return self.badness
 
 
-    #def measure(self, **pass_args):
-    #    """ Determine the current gradient """
-    #    self.M_hat = np.dot(self.W, self.V)
-    #    self.delta = self.f_delta(self.M, self.M_hat, **pass_args)
-    #    self.calc_badness()
-
-
     # TODO: Test. (esp. that offsets work.)
     def get_gradient(self, offsets=None, pass_args=None):
         """ 
         Calculate and return the current gradient.  
             offsets: 
-                Allowed values: None, (dW, dV)
-                    where dW and dV are is a W.shape and V.shape numpy arrays
-                Temporarily applies self.W += dW and self.V += dV before 
+                Allowed values: None, (dV, dW)
+                    where dV and dW are is a V.shape and W.shape numpy arrays
+                Temporarily applies self.V += dV and self.W += dW before 
                 calculating the gradient.
             pass_args:
                 Allowed values: dict of keyword arguments.
@@ -88,16 +90,16 @@ class HilbertEmbedder(object):
         # Determine the prediction for current embeddings.  Allow an offset to
         # be specified for solvers like Nesterov Accelerated Gradient.
         if offsets is not None:
-            use_V, use_W = self.temp_V, self.temp_W
+            use_W, use_V = self.temp_W, self.temp_V
             if not self.one_sided:
-                dW, dV = offsets
+                dV, dW = offsets
                 np.add(self.V, dV, use_V)
                 np.add(self.W, dW, use_W)
             else:
                 dV = offsets
                 np.add(self.V, dV, use_V)
         else:
-            use_V, use_W = self.V, self.W
+            use_W, use_V = self.W, self.V
 
         np.dot(use_W, use_V, self.M_hat)
 
@@ -109,7 +111,7 @@ class HilbertEmbedder(object):
         if not self.one_sided:
             np.dot(self.delta, use_V.T, self.nabla_W)
 
-        return self.nabla_W, self.nabla_V
+        return self.nabla_V, self.nabla_W
 
 
 

hilbert/test.py
@@ -328,7 +328,7 @@ class TestHilbertEmbedder(TestCase):
         expected_nabla_W = np.dot(delta, V.T)
         expected_nabla_V = np.dot(W.T, delta)
 
-        nabla_W, nabla_V = embedder.get_gradient()
+        nabla_V, nabla_W = embedder.get_gradient()
 
         self.assertTrue(np.allclose(nabla_W, expected_nabla_W))
         self.assertTrue(np.allclose(nabla_V, expected_nabla_V))
@@ -353,7 +353,7 @@ class TestHilbertEmbedder(TestCase):
         expected_nabla_W = np.dot(delta, V.T)
         expected_nabla_V = np.dot(W.T, delta)
 
-        nabla_W, nabla_V = embedder.get_gradient(offsets=(offset_W, offset_V))
+        nabla_V, nabla_W = embedder.get_gradient(offsets=(offset_V, offset_W))
 
         self.assertTrue(np.allclose(nabla_W, expected_nabla_W))
         self.assertTrue(np.allclose(nabla_V, expected_nabla_V))
@@ -379,7 +379,7 @@ class TestHilbertEmbedder(TestCase):
         delta = M - M_hat
         expected_nabla_V = np.dot(V, delta)
 
-        nabla_W, nabla_V = embedder.get_gradient()
+        nabla_V, nabla_W = embedder.get_gradient()
 
         self.assertTrue(np.allclose(nabla_V, expected_nabla_V))
         self.assertTrue(np.allclose(nabla_W, expected_nabla_V.T))
@@ -406,7 +406,7 @@ class TestHilbertEmbedder(TestCase):
         delta = M - M_hat
         expected_nabla_V = np.dot(V, delta)
 
-        nabla_W, nabla_V = embedder.get_gradient(offsets=offset_V)
+        nabla_V, nabla_W = embedder.get_gradient(offsets=offset_V)
 
         self.assertTrue(np.allclose(nabla_V, expected_nabla_V))
         self.assertTrue(np.allclose(nabla_W, expected_nabla_V.T))
@@ -913,6 +913,63 @@ class TestSolvers(TestCase):
 
         return params_expected
             
+
+
+class TestEmbedderSolverIntegration(TestCase):
+
+    def test_embedder_solver_integration(self):
+
+        d = 5
+        times = 3
+        learning_rate = 0.01
+        momentum_decay = 0.8
+        dictionary, N_xx, N_x = h.corpus_stats.get_test_stats(2)
+        M = h.corpus_stats.calc_positive_PMI(N_xx, N_x)
+
+        # This test just makes sure that the solver and embedder interface
+        # properly.  All is good as long as this doesn't throw errors.
+        embedder = h.embedder.HilbertEmbedder(
+            M, d, h.f_delta.f_mse, learning_rate)
+        solver = h.solver.NesterovSolver(
+            embedder, learning_rate, momentum_decay)
+        solver.cycle(times=times)
+
+
+    def test_embedder_nesterov_solver_optimized_integration(self):
+
+        d = 5
+        times = 3
+        learning_rate = 0.01
+        momentum_decay = 0.8
+        dictionary, N_xx, N_x = h.corpus_stats.get_test_stats(2)
+        M = h.corpus_stats.calc_positive_PMI(N_xx, N_x)
+
+        # This test just makes sure that the solver and embedder interface
+        # properly.  All is good as long as this doesn't throw errors.
+        embedder = h.embedder.HilbertEmbedder(
+            M, d, h.f_delta.f_mse, learning_rate)
+        solver = h.solver.NesterovSolverOptimized(
+            embedder, learning_rate, momentum_decay)
+        solver.cycle(times=times)
+
+
+    def test_embedder_momentum_solver_integration(self):
+
+        d = 5
+        times = 3
+        learning_rate = 0.01
+        momentum_decay = 0.8
+        dictionary, N_xx, N_x = h.corpus_stats.get_test_stats(2)
+        M = h.corpus_stats.calc_positive_PMI(N_xx, N_x)
+
+        # This test just makes sure that the solver and embedder interface
+        # properly.  All is good as long as this doesn't throw errors.
+        embedder = h.embedder.HilbertEmbedder(
+            M, d, h.f_delta.f_mse, learning_rate)
+        solver = h.solver.MomentumSolver(
+            embedder, learning_rate, momentum_decay)
+        solver.cycle(times=times)
+
 
 
 if __name__ == '__main__':