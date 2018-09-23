Make embedder.get_embedder interface with new decorated formulation of M. Start looking into sharding. Correct a problem with how gradient tracking was being done in CautiousNesterov Test get_embedder and get_M. Stop including pass_args in Embedder constructor. Start testing ideas for sharding.

hilbert/M.py
@@ -6,7 +6,6 @@ except ImportError:
     np = None
     torch = None
 
-NOTIFIED_DEPRECATED = False
 
 def apply_effects(base):
     def effected_base(
@@ -37,7 +36,6 @@ def apply_effects(base):
         return use_implementation(M, implementation, device)
 
     return effected_base
-
 
 
 @apply_effects
@@ -76,8 +74,8 @@ def calc_M_neg_samp(
         per corpus sample.  But if the value is provided, then that's what 
         is used as the weight.
     ``alpha`` (float): exponent applied to unigram distribution, which distorts
-        it.  As alpha becomes < 1, it makes the distribution flatter and 
-        more "temperate".  Conceptually, alpha is inverse temperature.
+        it.  As alpha becomes < 1, it makes the distribution flatter.
+        Conceptually, alpha is inverse temperature.
     """
     # Unpack args and apply defaults.
     k_weight = k_samples if k_weight is None else k_weight
@@ -94,6 +92,7 @@ def calc_M_neg_samp(
     # Draw negative samples
     distorted_px = distorted_Nx / distorted_N
     samples = sample_multi_multinomial(k_samples * Nx, distorted_px)
+    print('warning, negative sampling routine truncated for DEBUG')
 
     # Set negative sample wieght, if provided
     # Note that if k_weight is None, effective weight is k_samples
@@ -104,7 +103,6 @@ def calc_M_neg_samp(
         return np.log(Nxx) - np.log(samples)
 
 
-
 def sample_multi_multinomial(kNx, px):
     kNx = kNx.reshape(-1)
     px = px.reshape(-1)
@@ -112,7 +110,6 @@ def sample_multi_multinomial(kNx, px):
     for i in range(len(kNx)):
         samples[i,:] = np.random.multinomial(kNx[i], px)
     return samples
-
 
 
 def set_diag(M, val=None):
@@ -145,7 +142,7 @@ def undersample(cooc_stats, t=None):
     p_x = np.sqrt(t * cooc_stats.N / cooc_stats.Nx)
     p_x[p_x > 1] = 1
 
-    # Probability that, for a particular cooccurrent pair (i, j)
+    # Probability that, for a particular cooccurring pair (i, j)
     # both i and j are kept
     p_xx = p_x * p_x.T
 
@@ -161,7 +158,6 @@ def undersample(cooc_stats, t=None):
     return use_Nxx, use_Nx, use_N
 
 
-
 def use_implementation(M, implementation='torch', device='cuda'):
     h.utils.ensure_implementation_valid(implementation)
     if implementation == 'torch':
@@ -169,70 +165,24 @@ def use_implementation(M, implementation='torch', device='cuda'):
     elif implementation == 'numpy':
         return M
 
+
 def calc_M(
     cooc_stats,
     base,
-    shift=None,
-    no_neg_inf=False,
-    positive=False,
-    diag=None,
-    implementation='torch',
-    device='cuda'
+    *args,
+    **kwargs
 ):
-    """
-    Get the matrix of target dot-products.
-    `base` ('pmi' | 'logNxx' | 'swivel'): 
-        Determines the type of cooccurrence statistic to use as a base for M.
-    `shift` (None | float):
-        If `shift` is not None, add `log(shift)` to all values
-    `no_neg_inf` (False | True):
-        If True, set any negative infinity values to zero.
-    `positive` (False | True):
-        If `positive` is True, set all negative values to 0.
-    `diag` (None | float):
-        If `diag` is not None, set all diagonal cells to to the value of `diag`.
-    """
-
-    global NOTIFIED_DEPRECATED
-    if not NOTIFIED_DEPRECATED:
-        NOTIFIED_DEPRECATED = True
-        print(
-            'M.calc_M is deprecated.  '
-            'Start using one of the more specific M-calculators.'
-        )
-
-
-    # First, get the basic values for M
-    base = base.lower()
     if base == 'pmi':
-        M = h.corpus_stats.calc_PMI(cooc_stats)
-    elif base == 'lognxx':
-        with np.errstate(divide='ignore'):
-            M = np.log(cooc_stats.denseNxx)
-    elif base == 'swivel':
-        M = h.corpus_stats.calc_PMI_star(cooc_stats)
-    else:
-        raise ValueError('Unexpected `base` in get_M: %s' % repr(base))
-
-    if shift is not None:
-        M += shift
-
-    if no_neg_inf:
-        M[M==-np.inf] = 0
-
-    if positive:
-        M[M<0] = 0
-
-    if diag is not None:
-        np.fill_diagonal(M, diag)
-
-    if implementation == 'torch':
-        return torch.tensor(M, dtype=torch.float32, device=device)
-    elif implementation == 'numpy':
-        return M
+        return calc_M_pmi(cooc_stats, *args, **kwargs)
+    elif base == 'logNxx':
+        return calc_M_logNxx(cooc_stats, *args, **kwargs)
+    elif base == 'pmi-star':
+        return calc_M_pmi_star(cooc_stats, *args, **kwargs)
+    elif base == 'neg-samp':
+        return calc_M_neg_samp(cooc_stats, *args, **kwargs)
     else:
         raise ValueError(
-            'Unexpected `implementation` in get_M: %s.  Should be "torch" or '
-            '"numpy".' % repr(base)
+            "Unexpected base for calculating M: %s.  "
+            "Expected one of: 'pmi', 'logNxx', 'pmi-star', or 'neg-samp'."
         )
 

hilbert/embedder.py
@@ -20,7 +20,7 @@ def sim(word, context, embedder, dictionary):
 def get_embedder(
     cooc_stats,
     f_delta,            # 'mse' | 'w2v' | 'glove' | 'swivel' | 'mse'
-    base,               # 'pmi' | 'logNxx' | 'swivel'
+    base,               # 'pmi' | 'logNxx' | 'pmi-star' | 'neg-samp'
 
     solver='sgd',       # 'sgd' | 'momentum' | 'nesterov' | 'slosh'
 
@@ -29,19 +29,22 @@ def get_embedder(
     k=None,             # weight of negative samples (w2v only)
 
     # Options for M
-    shift=None,         # None | float -- shift all vals e.g. -np.log(k)
-    no_neg_inf=False,   # whether to set -np.inf values to zero.
-    positive=False,     # whether to clip negative values to zero.
-    diag=None,          # None | float -- set diagonals to this val
+    t_undersample=None,
+    shift_by=None,      # None | float -- shift all vals e.g. -np.log(k)
+    neg_inf_val=None,   # None | float -- set any negative infinities to given
+    clip_thresh=None,   # None | float -- clip values below thresh to thresh.
+    diag=None,          # None | float -- set main diagonal to given value.
 
-    # TODO: Add use noise samples, use noise smoothing
+    # Options for M if base is 'neg-samp':
+    k_samples=1,
+    k_weight=None,
+    alpha=1.0,
 
     # Options for embedder
     d=300,              # embedding dimension
-    learning_rate=1e-5,
+    learning_rate=1e-6,
     one_sided=False,    # whether vectors share parameters with covectors
     constrainer=None,   # constrainer instances can mutate values after update
-    pass_args={},       # kwargs to pass into f_delta when it is called
 
     # Options for solver
     momentum_decay=0.9,
@@ -52,10 +55,18 @@ def get_embedder(
 ):
 
     h.utils.ensure_implementation_valid(implementation)
-    M = h.M.calc_M(
-        cooc_stats, base, shift, no_neg_inf, positive, diag, 
-        implementation, device
-    )
+
+    M_args = {
+        'cooc_stats':cooc_stats, 'base':base, 't_undersample':t_undersample,
+        'shift_by':shift_by, 'neg_inf_val':neg_inf_val,
+        'clip_thresh':clip_thresh, 'diag':diag,
+        'implementation':implementation, 'device':device
+    }
+    if base == 'neg-samp':
+        M_args.update({
+            'k_samples':k_samples, 'k_weight':k_weight, 'alpha':alpha})
+    M = h.M.calc_M(**M_args)
+
     f_getter = (
         h.f_delta.get_f_MSE if f_delta=='mse' else
         h.f_delta.get_f_w2v if f_delta=='w2v' else
@@ -100,14 +111,20 @@ def get_embedder(
         device=device, **f_options
     )
 
+    # TODO: delegate validation of option combinations to a validation
+    #   subroutine
+    # TODO: stop supporting torch implementation here.
     if implementation == 'torch':
         embedder = h.torch_embedder.TorchHilbertEmbedder(
-            M, f_delta, d, learning_rate, one_sided, constrainer, pass_args,
-            device,
+            M=M, f_delta=f_delta, d=d, learning_rate=learning_rate, 
+            one_sided=one_sided, constrainer=constrainer,
+            device=device,
         )
     else:
         embedder = HilbertEmbedder(
-            M, f_delta, d, learning_rate, one_sided, constrainer, pass_args)
+            M=M, f_delta=f_delta, d=d, learning_rate=learning_rate, 
+            one_sided=one_sided, constrainer=constrainer,
+        )
 
     solver_instance = (
         embedder if solver=='sgd' else
@@ -140,7 +157,6 @@ class HilbertEmbedder(object):
         learning_rate=1e-6,
         one_sided=False,
         constrainer=None,
-        pass_args={}
     ):
         self.M = M
         self.d = d
@@ -154,7 +170,6 @@ class HilbertEmbedder(object):
         if self.one_sided and self.num_covecs != self.num_vecs:
             raise ValueError('M must be square for a one-sided embedder.')
         self.reset()
-        #self.measure(**pass_args)
 
 
     def sample_sphere(self):
@@ -264,7 +279,6 @@ class HilbertEmbedder(object):
 
 
     def cycle(self, times=1, print_badness=True, pass_args=None):
-        pass_args = pass_args or {}
         for i in range(times):
             self.update_self(pass_args)
             self.apply_constraints()

hilbert/solver.py
@@ -212,6 +212,7 @@ class NesterovSolverCautious(object):
 
 
     def allocate(self):
+        self.last_norm = None
         self.momenta = []
         self.updates = []
         self.last_gradient = []
@@ -231,35 +232,47 @@ class NesterovSolverCautious(object):
 
 
     def clear_momenta(self):
+        self.last_norm = None
         for j in range(len(self.momenta)):
             self.momenta[j][...] = 0
+            self.last_gradient[j][...] = 0
+            self.updates.append(np.zeros(param.shape))
 
 
     def cycle(self, times=1, pass_args=None):
         pass_args = pass_args or {}
-        
         for i in range(times):
 
             # Calculate gradients at accellerated position
             gradients = self.objective.get_gradient(pass_args=pass_args)
 
             # Calculate alignment with last gradient
-            norm = 0
-            last_norm = 0
-            product = 0
+            product = 0.
+            norm_squared = sum(
+                h.utils.norm(gradients[j])**2
+                for j in range(len(gradients))
+            )
+            product = sum( 
+                h.utils.dot(gradients[j], self.last_gradient[j])
+                for j in range(len(gradients))
+            )
 
-            for j in range(len(self.last_gradient)):
-                last_norm += h.utils.norm(self.last_gradient[j])
-                norm += h.utils.norm(gradients[j])
-                # TODO: can't use torch.dot this needs to generalize over np 
-                # arrays too.
-                product += h.utils.dot(gradients[j], self.last_gradient[j])
-
-            norms = norm * last_norm
-            if norms == 0:
+            norm = torch.sqrt(norm_squared)
+            if self.last_norm is None:
                 alignment = 1
+                norms = None
             else:
+                norms = self.last_norm * norm
                 alignment = product / norms
+
+            print('\tnorm: ' + str(norm))
+            print('\tlast_norm: ' + str(self.last_norm))
+            print('\tnorms: ' + str(norms))
+            print('\tproduct: ' + str(product))
+            if self.last_norm is not None:
+                print('\tproduct / norms: ' + str(product / norms))
+            print('\talignment: ' + str(alignment))
+            self.last_norm =  norm
 
             if self.implementation == 'torch':
                 self.last_gradient = [