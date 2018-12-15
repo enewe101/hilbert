e72c2df4448887501bb3a268ec1062a7ebb67942
Edward Newell <edward.newell@gmail.com>
2018-12-15 16:41:42 -0500

    added factories, tested. KL model exceeds memory on falcon

--- hilbert/factories.py
+++ hilbert/factories.py
@@ -103,3 +103,73 @@ def construct_glv_solver(
     )
     return embsolver
 
+
+
+def construct_max_likelihood_solver(*args, **kwargs):
+    """
+    This factory accepts the same set of arguments as
+    _construct_tempered_solver, except for sharder_class (which should not be
+    provided.
+    """
+    solver = _construct_tempered_solver(
+        h.msharder.MaxLikelihoodSharder, *args, **kwargs)
+    print('finished loading max-likelihood bad boi!')
+    return solver
+
+
+def construct_max_posterior_solver(*args, **kwargs):
+    """
+    This factory accepts the same set of arguments as
+    _construct_tempered_solver, except for sharder_class (which should not be
+    provided.
+    """
+    solver = _construct_tempered_solver(
+        h.msharder.MaxPosteriorSharder, *args, **kwargs)
+    print('finished loading max-posterior bad boi!')
+    return solver
+
+
+def construct_KL_solver(*args, **kwargs):
+    """
+    This factory accepts the same set of arguments as
+    _construct_tempered_solver, except for sharder_class (which should not be
+    provided.
+    """
+    solver = _construct_tempered_solver(h.msharder.KLSharder, *args, **kwargs)
+    print('finished loading KL bad boi!')
+    return solver
+
+
+def _construct_tempered_solver(
+    sharder_class,
+    bigram_path,
+    init_embeddings_path=None,
+    d=300,
+    temperature=1,
+    update_density=1.,
+    learning_rate=0.01,
+    opt_str='adam',
+    shard_factor=1,
+    seed=1,
+    device=None,
+):
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
+
+    # Make the bigram.
+    bigram = get_bigram(bigram_path)
+
+    # Now make the sharder.
+    sharder = sharder_class(bigram, temperature, update_density, device)
+
+    # Get initial embeddings.
+    init_vecs = get_init_embs(init_embeddings_path)
+
+    # Build the main daddyboi!
+    embsolver = h.autoembedder.HilbertEmbedderSolver(
+        sharder, get_opt(opt_str), d=d, learning_rate=learning_rate,
+        init_vecs=init_vecs, shape=None, one_sided=False,
+        learn_bias=False, shard_factor=shard_factor, seed=seed, device=device,
+    )
+    return embsolver
+
