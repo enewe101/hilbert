e9f0e3c34a0f9d5851247dde5d3be5d0739d3dca
Edward Newell <edward.newell@gmail.com>
2018-12-25 00:24:19 -0500

    MultiLoader working.  Preloader and gpu_loader are obsolete; follow the pattern made by MockLoader in test_cpu_loader

--- hilbert/cpu_loader.py
+++ hilbert/cpu_loader.py
@@ -1,64 +1,89 @@
 import hilbert as h
-from torch.multiprocessing import Queue, Process
+try:
+    import torch
+    from torch.multiprocessing import Queue, Process
+except ImportError:
+    torch = None
+    Queue, Process = None, None
 
 
 def construct_bigram_cpu_loader(shard_schedule, bigram_path, num_workers):
     preloaders = [
         h.preloader.BigramPreloader(bigram_path) for w in range(num_workers)]
-    return MultiCPULoader(shard_schedule, preloaders)
+    return MultiLoader(shard_schedule, preloaders, bigram_path)
 
 
 
-class CPULoader(object):
+class Loader(object):
 
     def __init__(self, shard_schedule):
         self.shard_schedule = shard_schedule
 
+
     def __iter__(self):
+        for shard_spec, cpu_tensors in self.preload_iter():
+            yield shard_spec, load(shard_spec, cpu_tensors)
+
+
+    def load(self, shard_spec, cpu_tensors):
         raise NotImplementedError(
-            'Subclasses should override `CPULoader.__iter__()`, and they '
-            'should do so by yielding `(shard_spec, cpu_data)` 2-tuples.'
+            'Subclasses should override `Loader.load()`, which must '
+            'return an iterable yielding pairs like `(shard_spec, gpu_data)`, '
+            'where `gpu_data` is some collection of gpu-tensors (e.g. tuple of '
+            'tensors, dictionary, or single tensor).'
         )
 
 
-class MultiCPULoader(CPULoader):
+    def preload_iter(self):
+        for shard_spec in shard_schedule:
+            yield shard_spec, self.preload(shard_spec)
 
-    def __init__(self, shard_schedule, preloaders):
+
+    def preload(self, shard_spec):
+        raise NotImplementedError(
+            'Subclasses should override `Loader.preload()`, which must '
+            'return an iterable yielding pairs like `(shard_spec, cpu_data)`, '
+            'where `cpu_data` is some collection of cpu-tensors (e.g. tuple of '
+            'tensors, dictionary, or single tensor).'
+        )
+
+
+
+
+
+class MultiLoader(Loader):
+
+    def __init__(self, shard_schedule, num_loaders, device=None):
         """
-        This CPULoader yields prepared cpu-loaded shards, which are a precursor
-        to gpu-loaded shards.  It prepares the cpu-loaded shards using worker
-        processes with which it shares memory.
+        Like `Loader`, `MultiLoader` is an iterable that yields `(shard_spec,
+        gpu_tensors)` pairs.  It preloads shards as cpu-tensors, so that the
+        gpu-tensors can be loaded in time to keep the GPU busy.
 
         INPUTS
         ``````
         `shard_schedule`    
             An iterator that yields shard_specs, which are
             2-tuples of `SliceObjects`
-
-        `preloaders`    
-            Instances satisfying the `Preloader` interface, which
-            build sets of tensors in shared memory 
-
-        The MultiCPULoader  is an iterator that yields `(shard_id,
-        cpu_tensors)` tuples, and hides the fact that workers in the background
-        prepare the next items for iteration.
-
-        Before entering into the iteration context, the  master puts shard IDs
-        on a queue.  Workers pull shard_IDs and assemble the cpu tensors, then
-        put them onto a `result_queue` where they are passed back to the 
-        main process by keeping the tensors in shared memory, and passing
-        references to them in a queue (thanks to `torch.multiprocessing`)
-
-        (The master process would then normally load them onto the GPU, and do
-        any preliminary GPU operations to finish preparing the shard.)
         """
         self.shard_schedule = shard_schedule
-        self.num_preloaders = len(preloaders)
-        self.preloaders = preloaders
-        self._start_preloaders()
+        self.num_loaders = num_loaders
+        self._start_preloading()
+        self.device = device
 
 
-    def _start_preloaders(self):
+    def setup(self, preloader_id):
+        """
+        This function is called once when running in a child process.  It allows
+        you to execute more __init__-like code, but to do so *after* being
+        transmitted through a pipe to a child process.  That's useful if you
+        need to load a lot of data, because you might as load it after 
+        transmitting through the pipe.  Also, it is useful for things not
+        likely to pickle well (e.g. a database connection?). 
+        """
+        pass
+
+
+    def _start_preloading(self):
         # The preloaders are responsible for building cpu-loaded shards in
         # background processes, so that they are ready to be loaded to the
         # GPU when they are needed.  This function starts the preloader
@@ -73,33 +98,55 @@ class MultiCPULoader(CPULoader):
         self.result_queue = Queue(maxsize=1)
 
         # Load the work.
-        for shard_id in self.shard_schedule:
-            self.request_queue.put(shard_id)
+        for shard_spec in self.shard_schedule:
+            self.request_queue.put(shard_spec)
 
         # Start all the loader processes
         self.loading_processes = []
-        for loader_id, preloader in enumerate(self.preloaders):
-            p = Process(
-                target=MultiCPULoader.preload,
-                args=(loader_id,self.request_queue,self.result_queue,preloader)
-            )
+        for loader_id in range(self.num_loaders):
+            p = Process(target=self.do_preloading, args=(loader_id,))
             p.start()
             self.loading_processes.append(p)
 
 
-    def __iter__(self):
+    def preload_iter(self):
         # Turn the result_queue into an iterator, and return it
         return h.utils.iterate_queue(
             self.result_queue, stop_when_empty=False, sentinal=StopIteration,
-            num_sentinals=self.num_preloaders
+            num_sentinals=self.num_loaders
         )
 
 
-    @staticmethod
-    def preload(loader_id, request_queue, result_queue, preloader):
-        preloader.setup(loader_id)
-        for shard_id in h.utils.iterate_queue(request_queue):
-            result_queue.put((shard_id, preloader[shard_id]))
-        result_queue.put(StopIteration())
+    def __iter__(self):
+        for shard_spec, cpu_tensors in self.preload_iter():
+            yield shard_spec, self.load(shard_spec, cpu_tensors)
 
+
+    def do_preloading(self, loader_id):
+        self.setup(loader_id)
+        for shard_spec in h.utils.iterate_queue(self.request_queue):
+            self.result_queue.put((shard_spec, self.preload(shard_spec)))
+        self.result_queue.put(StopIteration())
+
+
+
+
+
+class BigramLoader(MultiLoader):
+
+
+    def __init__(self, shard_schedule, preloaders, bigram_path, device=None):
+        self.bigram_path = bigram_path
+        super(MockLoader, self).__init__(shard_schedule, preloaders, device)
+
+    def setup(self, loader_id):
+        self.bigram = h.bigram.Bigram.load(self.bigram_path)
+
+    def load(self, shard_spec, cpu_data):
+        device = self.device or h.CONSTANTS.MATRIX_DEVICE
+        Nxx, Nx, Nxt, N = cpu_data
+        return Nxx.to(device), Nx.to(device), Nxt.to(device), N.to(device)
+
+    def preload(self, shard_spec):
+        return self.bigram.load_shard(shard=shard_spec, device='cpu')
 
--- hilbert/tests/test_cpu_loader.py
+++ hilbert/tests/test_cpu_loader.py
@@ -12,52 +12,89 @@ except ImportError:
     torch = None
 
 
-class PreloaderMock(h.preloader.Preloader):
-    def setup(self, loader_id):
-        self.loader_id = loader_id
+class MockLoader(h.cpu_loader.MultiLoader):
 
-    def __getitem__(self, shard_spec):
-        return self.loader_id, shard_spec
+    def __init__(self, shard_schedule, num_loaders, bigram_path, device=None):
+        self.bigram_path = bigram_path
+        super(MockLoader, self).__init__(shard_schedule, num_loaders, device)
+
+    def setup(self, loader_id):
+        self.bigram = h.bigram.Bigram.load(self.bigram_path)
+
+    def preload(self, shard_spec):
+        return self.bigram.load_shard(shard=shard_spec, device='cpu')
+
+    def load(self, shard_spec, cpu_data):
+        device = self.device or h.CONSTANTS.MATRIX_DEVICE
+        Nxx, Nx, Nxt, N = cpu_data
+        return Nxx.to(device), Nx.to(device), Nxt.to(device), N.to(device)
+
 
 
 class TestCPULoader(TestCase):
 
+    #def test_multi_cpu_loader(self):
+
+    #    bigram_path = os.path.join(h.CONSTANTS.TEST_DIR, 'bigram')
+    #    preloaders = [
+    #        h.preloader.BigramPreloader(bigram_path),
+    #        h.preloader.BigramPreloader(bigram_path)
+    #    ]
+    #    shard_schedule = h.shards.Shards(5)
+    #    cpu_loader = h.cpu_loader.MockLoader(
+    #        shard_schedule, preloaders, bigram_path)
+
+    #    expected_shards = list(shard_schedule)
+    #    expected_worker_ids = set(range(len(preloaders)))
+
+    #    for found_shard_spec, found_data in cpu_loader:
+    #        self.assertTrue(found_shard_spec in expected_shards)
+    #        worker_id, shard_spec_in_data = found_data
+    #        self.assertEqual(found_shard_spec, shard_spec_in_data)
+    #        self.assertTrue(worker_id in expected_worker_ids)
+
+
     def test_multi_cpu_loader(self):
 
-        preloaders = [PreloaderMock(), PreloaderMock()]
         shard_schedule = h.shards.Shards(5)
-        cpu_loader = h.cpu_loader.MultiCPULoader(shard_schedule, preloaders)
-
-        expected_shards = list(shard_schedule)
-        expected_worker_ids = set(range(len(preloaders)))
-
-        for found_shard_spec, found_data in cpu_loader:
-            self.assertTrue(found_shard_spec in expected_shards)
-            worker_id, shard_spec_in_data = found_data
-            self.assertEqual(found_shard_spec, shard_spec_in_data)
-            self.assertTrue(worker_id in expected_worker_ids)
-
-
-    def test_bigram_cpu_loader(self):
-
-        shard_schedule = h.shards.Shards(5)
-        num_workers = 2
         bigram_path = os.path.join(h.CONSTANTS.TEST_DIR, 'bigram')
-        cpu_loader = h.cpu_loader.construct_bigram_cpu_loader(
-            shard_schedule, bigram_path, num_workers)
+        num_loaders = 2
+        cpu_loader = MockLoader(shard_schedule, num_loaders, bigram_path)
 
         expected_bigram = h.bigram.Bigram.load(bigram_path)
         expected_shards = list(shard_schedule)
-
         num_shards_iterated = 0
         for found_shard_spec, found_data in cpu_loader:
             num_shards_iterated += 1
             self.assertTrue(found_shard_spec in expected_shards)
             expected_data = expected_bigram.load_shard(
-                found_shard_spec, device='cpu')
+                found_shard_spec, device=h.CONSTANTS.MATRIX_DEVICE)
             for found_tensor, expected_tensor in zip(found_data, expected_data):
                 self.assertTrue(torch.allclose(found_tensor, expected_tensor))
 
         self.assertEqual(num_shards_iterated, len(expected_shards))
+
+
+#    def test_bigram_cpu_loader(self):
+#
+#        shard_schedule = h.shards.Shards(5)
+#        num_workers = 2
+#        bigram_path = os.path.join(h.CONSTANTS.TEST_DIR, 'bigram')
+#        cpu_loader = h.cpu_loader.construct_bigram_cpu_loader(
+#            shard_schedule, bigram_path, num_workers)
+#
+#        expected_bigram = h.bigram.Bigram.load(bigram_path)
+#        expected_shards = list(shard_schedule)
+#
+#        num_shards_iterated = 0
+#        for found_shard_spec, found_data in cpu_loader:
+#            num_shards_iterated += 1
+#            self.assertTrue(found_shard_spec in expected_shards)
+#            expected_data = expected_bigram.load_shard(
+#                found_shard_spec, device='cpu')
+#            for found_tensor, expected_tensor in zip(found_data, expected_data):
+#                self.assertTrue(torch.allclose(found_tensor, expected_tensor))
+#
+#        self.assertEqual(num_shards_iterated, len(expected_shards))
 
 
