9714b1b1d9e66c014c6ffa6e921162e24b8449d4
Edward Newell <edward.newell@gmail.com>
2019-05-27 13:40:45 -0400

    remove hilbert_noshard

--- hilbert_noshard/CONSTANTS.py
+++
@@ -1,5 +0,0 @@
-from os import path
-CODE_DIR = path.abspath(path.join(__file__, '..'))
-TEST_DIR = path.join(CODE_DIR, 'test-data')
-TEST_TOKEN_PATH = path.join(CODE_DIR, 'test-data', 'test_doc.txt')
-TEST_DOCS_DIR = path.join(CODE_DIR, 'test-data', 'test-docs')
