Reorganized gradient accumulation in the solver. Fixed some typos.

hilbert/loader.py
@@ -237,7 +237,7 @@ class GibbsSampleLoader:
         self.gibbs_iteration = gibbs_iteration
 
         Nxx_data, I, J, Nx, Nxt = h.cooccurrence.CooccurrenceSector.load_coo(
-            cooccurrence_path, verbose=verbose, min_cooccurence_count=min_cooccurrence_count)
+            cooccurrence_path, verbose=verbose, min_cooccurrence_count=min_cooccurrence_count)
 
         # Calculate the probabilities and then temper them.
         # After tempering, probabilities are scores -- they don't sum to one
@@ -259,6 +259,21 @@ class GibbsSampleLoader:
 
         self.positive_sampler = Categorical(self.Pij, device=self.device)
 
+    def get_batch_words(self, batch_id, dictionary):
+        # help function for investigating problematic pairs of words
+        pos_pairs = []
+        neg_pairs = []
+
+        boundary = self.batch_size
+
+        for i, ij in enumerate(batch_id):
+            if i < boundary:
+                pos_pairs.append((dictionary.get_token(ij[0]), dictionary.get_token(ij[1])))
+            else:
+                neg_pairs.append((dictionary.get_token(ij[0]), dictionary.get_token(ij[1])))
+
+        return pos_pairs, neg_pairs
+
     def batch_probs(self, _2dprobs_prenorm, I_flag=True):
         """
         Help function for sampling negative samples by calculating the probabilities of the batch
@@ -271,7 +286,7 @@ class GibbsSampleLoader:
         else:
             # normalized
             _2dprobs = _2dprobs_prenorm/_2dprobs_prenorm.sum(dim=1)[:,None]
-            if torch.isnan(_2dprobs):
+            if torch.isnan(_2dprobs).any():
                 raise ValueError("detected nan in probs!")
             negative_sampler = torch.distributions.categorical.Categorical(_2dprobs)
             negative_samples_idx = negative_sampler.sample()    # sample 1 unit from the conditional distribution
@@ -295,11 +310,15 @@ class GibbsSampleLoader:
             # condition on words are from I, computing J given I
             model_pmi = self.learner.V[condition_on_idx] @ self.learner.W.t()
             _2d_posterior_dist = self.Pj * torch.exp(model_pmi)  # without Adaptive softmax
+            if torch.isnan(_2d_posterior_dist).any():
+                raise ValueError("In gibbs stepping, detected nan in probs!")
             negative_samples = self.batch_probs(_2d_posterior_dist, I_flag=True)
         else:
             # condition on words are from I, computing I given J
             model_pmi = self.learner.W[condition_on_idx] @ self.learner.V.t()
             _2d_posterior_dist = self.Pi * torch.exp(model_pmi)  # without Adaptive softmax
+            if torch.isnan(_2d_posterior_dist).any():
+                raise ValueError("In gibbs stepping, detected nan in probs!")
             negative_samples = self.batch_probs(_2d_posterior_dist, I_flag=False)
 
         return negative_samples
@@ -429,7 +448,7 @@ class CPUSampleLoader:
         temperature=1,
         batch_size=100000,
         device=None,
-        min_cooccurence_count = None,
+        min_cooccurrence_count = None,
         verbose=True
     ):
 

hilbert/solver.py
@@ -85,28 +85,15 @@ class Solver(object):
     def get_params(self):
         return self.learner.get_params()
 
-    def get_batch_words(self, batch_id):
-        pos_pairs = []
-        neg_pairs = []
 
-        boundary = self.loader.batch_size
-
-        for i, ij in enumerate(batch_id):
-            if i < boundary:
-                pos_pairs.append((self.dictionary.get_token(ij[0]), self.dictionary.get_token(ij[1])))
-            else:
-                neg_pairs.append((self.dictionary.get_token(ij[0]), self.dictionary.get_token(ij[1])))
-
-        return pos_pairs, neg_pairs
 
 
     def cycle(self, updates_per_cycle=1, monitor_closely=False):
-
+        grad_accumulation_step = self.gradient_accumulation
         # Run a bunch of updates.
         for update_id in range(updates_per_cycle):
             # Train on as many batches as the loader deems to be one update.
             for batch_id, batch_data in self.loader:
-                # IJ sample dictionary ID, None
                 # Consider this batch and learn.
                 response = self.learner(batch_id)
 
@@ -116,37 +103,23 @@ class Solver(object):
 
                 self.cur_loss = self.loss(response, batch_data)
                 self.cur_loss.backward()
-
-                if monitor_closely:
-                    self.V_norm = torch.norm(list(self.learner.parameters())[0].grad)
-                    self.W_norm = torch.norm(list(self.learner.parameters())[1].grad)
-                    print("\n\n")
-                    print("V grad norm", self.V_norm)
-                    print("W grad norm", self.W_norm)
-
+                grad_accumulation_step = grad_accumulation_step - 1
 
                 if self.gradient_clipping is not None:
-                    # Gradient clipping
                     torch.nn.utils.clip_grad_norm_(self.learner.parameters(), max_norm=self.gradient_clipping)
 
                 if monitor_closely:
-                    try:
-                        if self.cur_loss.item() > 1e4:
-                            # print("The last loss is: ", self.cur_loss)
-                            pos_pairs, neg_pairs = self.loader.get_batch_words(batch_id)
+                    if self.cur_loss.item() > 1e4:
+                        pos_pairs, neg_pairs = self.loader.get_batch_words(batch_id, self.dictionary)
 
-                            UserWarning("Extreme loss value is detected. current loss is greater than 1e4,\n"
-                                        "Positive sample word pairs are :{}\n"
-                                        "Negative sample word pairs are :{}\n".format(", ".join(map(str, pos_pairs)),
-                                                                                      ", ".join(map(str, neg_pairs))))
-                    except NameError:
-                        pass
+                        UserWarning(
+                            "Extreme loss value is detected. current loss is greater than 1e4,\n"
+                            "Positive sample word pairs are :{}\n"
+                            "Negative sample word pairs are :{}\n".format(
+                                ", ".join(map(str, pos_pairs)),
+                                ", ".join(map(str, neg_pairs))))
 
-                # Take some steps
-
-                if (update_id + 1) % self.gradient_accumulation == 0\
-                        or (update_id + 1) == updates_per_cycle:
-                    # Gradient accumulation
+                if grad_accumulation_step == 0:
                     self.optimizer.step()
                     self.optimizer.zero_grad()
 
@@ -165,7 +138,11 @@ class Solver(object):
                 if monitor_closely:
                     tracer.declare('loss', self.cur_loss.item())
 
+            # don't waste the gradient!
+            self.optimizer.step()
+            self.optimizer.zero_grad()
             tracer.declare('loss', self.cur_loss.item())
+
         return self.cur_loss.item()
 
 